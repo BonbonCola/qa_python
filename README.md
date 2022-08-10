ba59bf5cef6718e22e16772cecfe92d487ae3096
Olga Zyuzyakina <olga.zyuzyakina@yandex.ru>
2022-08-10 14:58:23 +0400

    added tests for add_new_book and set_book_rating methods

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # qa_python
+qa_python - это второй проект в практикуме, он посвящен юнит-тестам.
+
+## Тесты
+```python
+test_add_new_book_add_two_books_successful(self):
+```
+проверяет добвление двух разных книг в словарь `books_rating` методом `add_new_book()`
+```python
+test_add_new_book_add_book_already_exist_not_added(self):
+```
+проверяет, что одну и ту же книгу можно добавить  в словарь `books_rating` только 1 раз методом `add_new_book()`
+```python
+test_add_new_book_new_book_have_rating_1(self):
+```
+проверяет, что у добавленной методом `add_new_book()` в словарь `books_rating` книги рейтинг по умолчанию равен 1
+```python
+test_set_book_rating_set_rating_5_success(self):
+```
+проверяет, что метод `set_book_rating()` устанавливает валидный рейтинг книги
+```python
+test_set_book_rating_invalid_rating_not_set(self, invalid_rating):
+```
+проверяет, что метод `set_book_rating()` не устанавливает не валидный рейтинг книги (значения параметров 0 и 11)
+```python
+test_set_book_rating_if_book_not_exist_valid_rating_not_set(self):
+```
+проверяет, что метод `set_book_rating()` не устанавливает валидный рейтинг несуществующей книге и не создает ее
+
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from main import BooksCollector
+import pytest
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -8,7 +10,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books_successful(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -22,3 +24,33 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_book_already_exist_not_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_add_new_book_new_book_have_rating_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.books_rating['Гордость и предубеждение и зомби'] == 1
+
+    def test_set_book_rating_set_rating_5_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+        assert collector.books_rating['Гордость и предубеждение и зомби'] == 5
+
+    @pytest.mark.parametrize('invalid_rating', [0, 11])
+    def test_set_book_rating_invalid_rating_not_set(self, invalid_rating):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', invalid_rating)
+        assert collector.books_rating['Гордость и предубеждение и зомби'] == 1
+
+    def test_set_book_rating_if_book_not_exist_valid_rating_not_set(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Тестовая книга', 5)
+        assert collector.books_rating.get('Тестовая книга') is None
+
